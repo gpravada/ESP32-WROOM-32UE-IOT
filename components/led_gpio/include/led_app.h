0e5624ddf6a9b23aaf5a01ed437f33b166c7e8a6
gravada <gravada@neptunetg.com>
2023-08-30 10:32:45 +0530

    Add cmp280 and led

---
+++ components/led_gpio/include/led_app.h
@@ -0,0 +1,9 @@
+/*! \file led_app.h
+    \brief Led blink header file */
+#ifndef __LED_APP_H__
+#define __LED_APP_H__
+
+void led_init();
+void led_blink_task(void *ignore);
+
+#endif
---
+++ components/led_gpio/led_app.c
@@ -0,0 +1,39 @@
+/*
+****************************************************************************
+* Copyright (C) 2023 - QualiZeal
+*
+* bme_app.c
+* Date: 2016/07/04
+* Revision: 2.0.5(Pressure and Temperature compensation code revision is 1.1
+*               and Humidity compensation code revision is 1.0)
+*
+* Usage: Sensor Driver interface file for BME280 sensor
+*
+****************************************************************************/
+
+#include "led_app.h"
+
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "freertos/task.h"
+
+#define LED_GPIO_PIN 5
+
+void led_init()
+{
+    esp_rom_gpio_pad_select_gpio(LED_GPIO_PIN);
+    gpio_set_direction(LED_GPIO_PIN, GPIO_MODE_OUTPUT);
+}
+
+void led_blink_task(void *ignore)
+{
+    int led_state = 1u;
+
+    while(true)
+    {
+        led_state = !led_state;
+        gpio_set_level(LED_GPIO_PIN, led_state);
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+    }
+}
